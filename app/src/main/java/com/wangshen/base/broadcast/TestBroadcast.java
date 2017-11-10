72ea7f705adbb1456c2c7809b60b866abf595829
wangwenzhang1 <wwz19900222@163.com>
2017-11-10 17:22:42 +0800

    整个项目包括文件夹，加了一些Test类只是为了把文件夹传上

---
+++ app/src/main/java/com/wangshen/base/broadcast/TestBroadcast.java
@@ -0,0 +1,8 @@
+package com.wangshen.base.broadcast;
+
+/**
+ * 创建者： wangwenzhang 时间： 2017/11/10.
+ */
+
+public class TestBroadcast {
+}
---
+++ app/src/main/java/com/wangshen/base/service/TestService.java
@@ -0,0 +1,8 @@
+package com.wangshen.base.service;
+
+/**
+ * 创建者： wangwenzhang 时间： 2017/11/10.
+ */
+
+public class TestService {
+}
